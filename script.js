9d38a0134926df258bae14d2ddd022cb702a5b1f
CeleMyBeloved1404 <Tadeo.12972@gmail.com>
2025-10-08 18:02:42 -0300

    update pages

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-  event.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
-
-  const subject = `Nuevo mensaje de ${name}`;
-  const body = `De: ${name} (${email})%0D%0A%0D%0A${message}`;
-
-  window.location.href = `mailto:tuemail@gmail.com?subject=${subject}&body=${body}`;
-
-  document.getElementById("status").innerText = "Abriendo cliente de correo...";
-});
